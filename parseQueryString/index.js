239ce302caa17aad7bf184278fc94737a9357c6e
Frida <yanmenghan1995@163.com>
2021-02-25 01:04:10 +0800

    add querystring

--- parseQueryString/index.js
+++ parseQueryString/index.js
@@ -1,1 +1,25 @@
 // 手写queryString.parse
+
+// http://www.baidu.com?a=miaomiao&b=wangwang
+
+function parseQueryString(url) {
+  let _url = new URL(url);
+  let obj = {};
+
+  for (let i of _url.searchParams) {
+    const [_k, _v] = i;
+
+    let k = decodeURIComponent(_k);
+    let v = decodeURIComponent(_v);
+
+    if (obj[k] !== undefined) {
+      obj[k] = [].concat(obj[k], v);
+    } else {
+      obj[k] = v;
+    }
+  }
+  return obj;
+}
+parseQueryString(
+  "http://www.baidu.com?a=miaomiao&b=wangwang&code=20180711%23abc&a=lalala"
+);
